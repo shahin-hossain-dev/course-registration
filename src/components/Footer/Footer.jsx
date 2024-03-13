89f893e44889c1a034b55f8f8a093918b438398b
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-03-14 01:00:23 +0600

    footer updatd

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="py-10">
       <p className="text-center">
-        &copy;2024 all rights Reserved by Shahin Hossain
+        &copy;2024 All rights reserved by Shahin Hossain
         <FaReact className="inline text-sky-500 text-2xl ms-2" />
       </p>
     </div>
